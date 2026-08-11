bfec2d0cbd6e571ce99fbd0cb12bbcf8c8df4626
Bhumika1503 <rajputbhumika15@gmail.com>
2026-08-11 22:39:35 +0530

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.java
@@ -0,0 +1,61 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    private TrieNode root = new TrieNode();
+    private List<String> result = new ArrayList<>();
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        for (String word : words) {
+            TrieNode node = root;
+
+            for (char ch : word.toCharArray()) {
+                int idx = ch - 'a';
+
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+
+                node = node.children[idx];
+            }
+
+            node.word = word;
+        }
+
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                dfs(board, r, c, root);
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(char[][] board, int r, int c, TrieNode node) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) {
+            return;
+        }
+        char ch = board[r][c];
+        if (ch == '#') {
+            return;
+        }
+        TrieNode next = node.children[ch - 'a'];
+        if (next == null) {
+            return;
+        }
+        if (next.word != null) {
+            result.add(next.word);
+
+            next.word = null;
+        }
+        board[r][c] = '#';
+        dfs(board, r + 1, c, next);
+        dfs(board, r - 1, c, next);
+        dfs(board, r, c + 1, next);
+        dfs(board, r, c - 1, next);
+        board[r][c] = ch;
+    }
+}
